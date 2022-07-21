764a798d54ac7f0d7eff15ea39ea6925dc046865
Pavankalyan1611 <pkkoppisetti@gmail.com>
2022-07-21 21:23:22 +0530

    [pavan] Creating Simple Linked List

---
+++ Day14_LinkedListPrblm/Day14_LinkedListPrblm/LinkedList.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day14_LinkedListPrblm
+{
+    public class LinkedList
+    {
+        public Node head;
+
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+                this.head = node;
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                    temp = temp.next;
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into the linked list", node.data);
+        }
+        public void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Linked list is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data + "->");
+                temp = temp.next; 
+            }
+
+        }
+    }
+}
---
+++ Day14_LinkedListPrblm/Day14_LinkedListPrblm/Program.cs
@@ -0,0 +1,9 @@
+﻿using Day14_LinkedListPrblm;
+
+Console.WriteLine("Welcome to Data Structure Linked List :\n");
+LinkedList list = new LinkedList();
+list.Add(56);
+list.Add(30);
+list.Add(70);
+Console.Write("\nLinked List : ");
+list.Display();
